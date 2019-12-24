43c15c889db1043dfb62e634c75897fe42bd2dc3
Lethe201 <qwer1234>
2019-12-24 22:15:23 +0900

    PATH

--- LDA/LDA.py
+++ LDA/LDA.py
@@ -1,9 +1,9 @@
 from scipy.io import arff
 import pandas as pd
-
+PATH='./LDA/'
 
 # dataset from https://archive.ics.uci.edu/ml/datasets/EEG+Eye+State#
-data_fn="./LDA/EEG_Dataset.arff"
-data = arff.loadarff(data_fn)
+data_fn="EEG_Dataset.arff"
+data = arff.loadarff(PATH+data_fn)
 df=pd.DataFrame(data[0])
 
